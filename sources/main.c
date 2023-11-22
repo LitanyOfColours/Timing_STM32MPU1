d082d1f35a021d97126e6f233af686f01cfe194c
RandomCodeGenerator <xkouri12@vutbr.cz>
2023-11-22 14:30:53 +0100

    Comment cleanup

--- sources/main.c
+++ sources/main.c
@@ -67,8 +67,9 @@ void *thread_func(void *data)
     
     while(1)
     {
-        clock_gettime(CLOCK_MONOTONIC, &time_keeper);
-        printf("I am still alive at time: %ld\n", time_keeper.tv_nsec/1000);
+        //Debug print
+        //clock_gettime(CLOCK_MONOTONIC, &time_keeper);
+        //printf("I am still alive at time: %ld\n", time_keeper.tv_nsec/1000);
 
         if(!waiting)
             err = write(fd, msg, sizeof(msg));
@@ -97,79 +98,7 @@ void *thread_func(void *data)
         inc_period(&wait_time); 
         clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &wait_time.next_period, NULL);
     }
-    /*
-    while(1)
-    {
-
-        int err = read(fd,&buffer,sizeof(buffer));
-        
-        if (err != -1)
-        {
-            clock_gettime(CLOCK_MONOTONIC, &time_read);
-
-            printf("RTT [us]: %ld \n", (time_read.tv_nsec - time_write.tv_nsec)/1000);
-
-            break;
-        }
-
-    }*/
-        /* Do RT specific stuff here */
-    /*
-
-
-    while(1)
-    {
-        
-        //write a simple message and its length
-        //int err =    write(fd, msg, sizeof(msg));
-
-        while(1)
-        {
-
-            int err = write(fd, msg, sizeof(msg));
-
-            #ifdef DEBUG_MESSAGES
-            printf("Attempting write, err: %d\n",err);
-            #endif
-            
-            if (err != -1)
-            {
-                clock_gettime(CLOCK_MONOTONIC, &time_write);
-                #ifdef DEBUG_MESSAGES
-                printf(buffer);
-                printf("\n");
-                #endif
-                
-                break;
-            }
-
-        }
-
-        while(1)
-        {
-
-            int err = read(fd,&buffer,sizeof(buffer));
-
-            #ifdef DEBUG_MESSAGES
-            printf("Attempting read, err: %d \n", err);
-            #endif
-            
-            if (err != -1)
-            {
-                clock_gettime(CLOCK_MONOTONIC, &time_read);
-                #ifdef DEBUG_MESSAGES
-                printf(buffer);
-                #endif
-
-                printf("RTT [us]: %ld \n", (time_read.tv_nsec - time_write.tv_nsec)/1000);
-
-                break;
-            }
-
-        }
-    }
-    */
-
+ 
     //close the device
     close(fd);
     return NULL;
@@ -235,83 +164,7 @@ int main(int argc, char * argv [])
             printf("join pthread failed: %m\n");
  
 out:
-    printf("Done all I could");
+    //printf("Done all I could"); //Debug print
     return ret;
-/*
-    struct timespec time_keeper;
-    struct timespec time_write;
-    struct timespec time_read;
-
-    clock_gettime(CLOCK_REALTIME, &time_keeper);
-    printf("Time of start [us]: %ld \n",time_keeper.tv_nsec/1000);
-    char msg [] = "echo";
-    char buffer [50];
-
-
-    //open the device
-    int fd = open(TTY_RPMSG0, O_RDWR | O_NOCTTY | O_SYNC | O_NDELAY);
-
-
-    while(1)
-    {
-        
-        //write a simple message and its length
-        //int err =    write(fd, msg, sizeof(msg));
-
-        #ifdef DEBUG_MESSAGES
-        printf("Attempting write, err: %d\n",err );
-        #endif
-
-        while(1)
-        {
-
-            int err = write(fd, msg, sizeof(msg));
-
-            #ifdef DEBUG_MESSAGES
-            printf("Attempting write, err: %d\n",err);
-            #endif
-            
-            if (err != -1)
-            {
-                clock_gettime(CLOCK_REALTIME, &time_write);
-                #ifdef DEBUG_MESSAGES
-                printf(buffer);
-                printf("\n");
-                #endif
-                
-                break;
-            }
-
-        }
-
-        while(1)
-        {
-
-            int err = read(fd,&buffer,sizeof(buffer));
-
-            #ifdef DEBUG_MESSAGES
-            printf("Attempting read, err: %d \n", err);
-            #endif
-            
-            if (err != -1)
-            {
-                clock_gettime(CLOCK_REALTIME, &time_read);
-                #ifdef DEBUG_MESSAGES
-                printf(buffer);
-                #endif
-
-                printf("RTT [us]: %ld \n", (time_read.tv_nsec - time_write.tv_nsec)/1000);
-
-                break;
-            }
-
-        }
-    }
-
-
-    //close the device
-    close(fd);
-
-    return 0;*/
 
 }
